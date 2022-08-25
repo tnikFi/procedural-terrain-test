09efde0ab6e546c34075aba4ed5293f0de5e2e34
= <=>
2022-08-25 17:48:32 +0300

    Improved texture generation

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -2,7 +2,14 @@
 using UnityEngine;
 
 public class Generator : MonoBehaviour {
+    
+    public enum DrawMode {
+        noise,
+        color
+    }
 
+    public DrawMode drawMode;
+    
     public int mapWidth;
     public int mapHeight;
     public int seed;
@@ -14,11 +21,30 @@ public class Generator : MonoBehaviour {
     public Vector2 offset;
     public bool autoUpdate;
 
+    public TerrainType[] TerrainTypes;
+    
     public void Generate() {
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
+        Color[] colorMap = new Color[mapWidth * mapHeight];
+        for (int y = 0; y < mapHeight; y++) {
+            for (int x = 0; x < mapWidth; x++) {
+                float height = noiseMap[x, y];
+                for (int i = 0; i < TerrainTypes.Length; i++) {
+                    if (height < TerrainTypes[i].height) {
+                        colorMap[y * mapWidth + x] = TerrainTypes[i].color;
+                        break;
+                    }
+                }
+            }
+        }
+        
         MapDisplay display = FindObjectOfType<MapDisplay>();
-        display.DrawNoiseMap(noiseMap);
+        if (drawMode == DrawMode.noise) {
+            display.DrawTexture(TextureGenerator.TextureFromNoiseMap(noiseMap));
+        } else if (drawMode == DrawMode.color) {
+            display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+        }
     }
 
     private void OnValidate() {
@@ -38,4 +64,11 @@ public class Generator : MonoBehaviour {
             lacunarity = 1;
         }
     }
+}
+
+[System.Serializable]
+public struct TerrainType {
+    public string name;
+    public float height;
+    public Color color;
 }

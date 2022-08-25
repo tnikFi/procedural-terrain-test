f7e33110b54f7235811bfeaeb7210dbb0b7e49df
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-08-25 17:59:27 +0300

    Changed texture filtering mode

--- Assets/Scripts/TextureGenerator.cs
+++ Assets/Scripts/TextureGenerator.cs
@@ -3,6 +3,8 @@
 public static class TextureGenerator {
     public static Texture2D TextureFromColorMap(Color[] colorMap, int width, int height) {
         Texture2D texture = new Texture2D(width, height);
+        texture.filterMode = FilterMode.Point;
+        texture.wrapMode = TextureWrapMode.Clamp;
         texture.SetPixels(colorMap);
         texture.Apply();
         return texture;
